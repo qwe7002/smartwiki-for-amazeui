23c446f05ab09bef89e8818950790e253916d9c0
qwe7002 <qwe7002@qwe7002deMacBook-Air.local>
2015-02-14 10:56:09 +0800

    添加文件

---
+++ index.php
@@ -0,0 +1,116 @@
+<?php
+include("conf.php");
+spl_autoload_register(function($class){
+  require preg_replace('{\\\\|_(?!.*\\\\)}', DIRECTORY_SEPARATOR, ltrim($class, '\\')).'.php';
+});
+
+use \Michelf\MarkdownExtra;
+if(isset($_GET['page'])){
+$pages=$_GET['page'];
+}else{
+  $pages="index";
+}
+if(!file_exists("file/".$pages.".md")){
+die("404 Not Found");
+}else{
+ $text = file_get_contents("file/".$pages.".md");
+ $html = MarkdownExtra::defaultTransform($text);
+     }
+?>
+<!doctype html>
+<html class="no-js">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title><?php echo $filesetting[$pages]['heading'] ?> - <?php echo $Gsetting["title"]?></title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <meta name="renderer" content="webkit">
+  <meta http-equiv="Cache-Control" content="no-siteapp" />
+  <meta name="apple-mobile-web-app-title" content="" />
+  <link rel="stylesheet" href="assets/css/amazeui.min.css"/>
+  <link rel="stylesheet" href="assets/css/admin.css">
+</head>
+<body>
+<!--[if lte IE 9]>
+<p class="browsehappy">你正在使用<strong>过时</strong>的浏览器，Amaze UI 暂不支持。 请 <a href="http://browsehappy.com/" target="_blank">升级浏览器</a>
+  以获得更好的体验！</p>
+<![endif]-->
+
+<header class="am-topbar admin-header">
+  <div class="am-topbar-brand">
+<strong><?php echo $Gsetting["title"]?></strong>
+  </div>
+
+  <button class="am-topbar-btn am-topbar-toggle am-btn am-btn-sm am-btn-success am-show-sm-only" data-am-collapse="{target: '#topbar-collapse'}"><span class="am-sr-only">导航切换</span> <span class="am-icon-bars"></span></button>
+
+  <div class="am-collapse am-topbar-collapse" id="topbar-collapse">
+
+    <ul class="am-nav am-nav-pills am-topbar-nav am-topbar-right admin-header-list">
+    </ul>
+  </div>
+</header>
+
+<div class="am-cf admin-main">
+  <!-- sidebar start -->
+  <div class="admin-sidebar am-offcanvas" id="admin-offcanvas">
+    <div class="am-offcanvas-bar admin-offcanvas-bar">
+      <ul class="am-list admin-sidebar-list">
+        <?PHP
+            foreach ($filesetting as $key => $val)
+            {
+             if (is_array ($val))
+             {
+              if($key==$pages){
+              echo '<li class= "active"><a href="?page='.$key.'">'.$val["heading"].'</a></li>';
+              }else{
+              echo '<li><a href="?page='.$key.'">'.$val["heading"].'</a></li>';
+             }
+             }
+            }
+            ?>
+      </ul>
+
+    </div>
+  </div>
+  <!-- sidebar end -->
+
+  <!-- content start -->
+  <div class="admin-content">
+
+    <div class="am-cf am-padding">
+      <div class="am-fl am-cf"><strong class="am-text-primary am-text-lg"><?PHP echo $filesetting[$pages]['heading'];?></strong> / <small><?PHP echo $filesetting[$pages]['subheading'];?></small></div>
+    </div>
+
+    <div class="am-g">
+      <div class="am-u-sm-12">
+       <?php echo $html; ?>
+      </div>
+    </div>
+  </div>
+  <!-- content end -->
+</div>
+
+<a class="am-icon-btn am-icon-th-list am-show-sm-only admin-menu" data-am-offcanvas="{target: '#admin-offcanvas'}"></a>
+
+<footer>
+  <hr>
+  <p class="am-padding-left"><?php echo $Gsetting['foottitle']?></p>
+</footer>
+
+<!--[if lt IE 9]>
+<script src="http://libs.baidu.com/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://cdn.staticfile.org/modernizr/2.8.3/modernizr.js"></script>
+<script src="assets/js/polyfill/rem.min.js"></script>
+<script src="assets/js/polyfill/respond.min.js"></script>
+<script src="assets/js/amazeui.legacy.js"></script>
+<![endif]-->
+
+<!--[if (gte IE 9)|!(IE)]><!-->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/amazeui.min.js"></script>
+<!--<![endif]-->
+<script src="assets/js/app.js"></script>
+</body>
+</html>
